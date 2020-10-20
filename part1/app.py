03f2028d3e7040ff0fcfc05ec7dda31ad8bf1b07
Nickolay Demidov <nickolay.demidov@Mac-mini-Nickolay.local>
2020-10-20 19:05:40 +0300

    #9 login.html + redirect

--- part1/app.py
+++ part1/app.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, redirect, session
 
 app = Flask(__name__)
-# для активации session необходимо:
+
 app.config['SECRET_KEY'] = 'abracadabra'
-# session используется для хранение flash, которые
-# могут быть извлечены get_flashed_messages()
 
 menu = [{"name": "Установка", "url": "install-flask"},
         {"name": "Приложение", "url": "first-app"},
@@ -34,6 +32,24 @@ def contact():
             flash('Ошибка отправки', category='error')
 
     return render_template('contact.html', title='Обратная связь', menu=menu), 404
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    return f"Пользователь: {username}"
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if "userLogged" in session:
+        return redirect(url_for("profile", username=session["userLogged"]))
+    elif request.method == "POST" and request.form["username"] == "nick" and request.form["psw"] == "1234":
+        session["userLogged"] = request.form["username"]
+        return redirect(url_for("profile", username=session["userLogged"]))
+
+    return render_template("login.html", title="Авторизация", menu=menu)
+
+
 
 # обработчик ошибок
 @app.errorhandler(404)
