b547898f9b5eec0bf9c22bb482a928319b242d6d
Nickolay Demidov <nickolay.demidov@Mac-mini-Nickolay.local>
2020-10-20 17:46:25 +0300

    #6 web-form

--- part1/app.py
+++ part1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -17,6 +17,15 @@ def index():
 def about():
     print(url_for('about'))
     return render_template('about.html', title='О сайте', menu=menu)
+
+
+@app.route('/contact', methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        print(request.form)
+        print(request.form['username'])
+
+    return render_template('contact.html', title='Обратная связь', menu=menu)
 
 
 if __name__ == '__main__':
