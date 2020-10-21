69c65a0e1b9c5909a51eee0cc8ea00bb8f192490
Nickolay Demidov <nickolay.demidov@Mac-mini-Nickolay.local>
2020-10-21 12:47:54 +0300

    #12 DB connection

--- part2/app.py
+++ part2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 import sqlite3
 import os
 
@@ -13,25 +13,34 @@ app.config.from_object(__name__)
 app.config.update(dict(DATABASE=os.path.join(app.root_path, "site.db")))
 
 
-# создание БД (из конфигурации нашего приложения)
 def connect_db():
     conn = sqlite3.connect(app.config['DATABASE'])
     conn.row_factory = sqlite3.Row  # записи в БД будут иметь вид не кортежа, а в виде словаря
     return conn
 
 
-# Запуск создания БД с таблицами (без запуска веб-сервера)
 def create_db():
+    """Вспомогательная функция для создания таблиц БД"""
     db = connect_db()
     with app.open_resource('sql_code.sql', mode='r') as f:
         db.cursor().executescript(f.read())
     db.commit()
     db.close()
-# создание БД без поднятия сервера
-# Python Console
-# >>> from app import create_db
-# >>> create_db()
 
 
-# if __name__ == "__main__":
-#     app.run()
+def get_db():
+    """Соединение с БД, если оно еще не установлено"""
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.route("/")
+def index():
+    db = get_db()
+    return render_template("index.html", menu=[])
+
+
+
+if __name__ == "__main__":
+    app.run()
