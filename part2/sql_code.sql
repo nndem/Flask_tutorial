332d7cc7f4ba5657ab693ee0e9c126141833e54b
Nickolay Demidov <nickolay.demidov@Mac-mini-Nickolay.local>
2020-10-21 14:17:55 +0300

    #14 add new table

--- part2/sql_code.sql
+++ part2/sql_code.sql
@@ -3,3 +3,10 @@ id integer PRIMARY KEY AUTOINCREMENT,
 title text NOT NULL,
 url text NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS posts (
+id integer PRIMARY KEY AUTOINCREMENT,
+title text NOT NULL,
+text text NOT NULL,
+time integer NOT NULL
+);
