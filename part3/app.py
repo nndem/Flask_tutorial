31d829a20466d07357020137cc6b25c27af57efb
Nickolay Demidov <nickolay.demidov@Mac-mini-Nickolay.local>
2020-10-22 18:23:50 +0300

    #34 response catching decorators

--- part3/app.py
+++ part3/app.py
@@ -23,5 +23,27 @@ def transfer():
     return redirect(url_for("index"), 301)
 
 
+@app.before_first_request
+def before_first_request():
+    print("before_first_request() called")
+
+
+@app.before_request
+def before_request():
+    print("before_request() called")
+
+
+@app.after_request
+def after_request(response):
+    print("after_request() called")
+    return response
+
+
+@app.teardown_request
+def teardown_request(response):
+    print("teardown_request() called")
+    return response
+
+
 if __name__ == '__main__':
     app.run()
