1ce1a28a4664be21ac307af9de9ba132d78c6c8f
Nickolay Demidov <nickolay.demidov@Mac-mini-Nickolay.local>
2020-10-23 15:06:54 +0300

    #43 create users-table

--- part2/sql_code.sql
+++ part2/sql_code.sql
@@ -12,3 +12,10 @@ url text NOT NULL,
 time integer NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS users (
+id integer PRIMARY KEY AUTOINCREMENT,
+name text NOT NULL,
+email text NOT NULL,
+psw text NOT NULL,
+time int NOT NULL
+);
