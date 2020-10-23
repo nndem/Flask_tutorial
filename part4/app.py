4e44647a1b757639a89319228cde0c06c563c593
Nickolay Demidov <nickolay.demidov@Mac-mini-Nickolay.local>
2020-10-23 12:47:06 +0300

    #38 session v2

--- part4/app.py
+++ part4/app.py
@@ -4,13 +4,18 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "1213377564b1ff2de1f8e24caddea9f73736cfa1"
 
 
+data = [1, 2, 3, 4]
+
+
+# Когда в session храним несколько объектов:
 @app.route("/")
 def index():
-    if "visits" in session:
-        session["visits"] = session.get("visits") + 1  # обновление данных в сессии
+    if "data" in session:
+        session["data"][0] += 1  # модифицирование данных
+        session.modified = True
     else:
-        session["visits"] = 1  # запись данных в сессию
-    return f"<h1>Main Page</h1><p>Число просмотров: {session['visits']}"
+        session["data"] = data  # запись данных в сессию
+    return f"<h1>Main Page</h1><p>Данные в сессии: {session['data']}"
 
 
 if __name__ == '__main__':
