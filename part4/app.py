d77de1684d8330405c459fc4eaf2a27aab660b34
Nickolay Demidov <nickolay.demidov@Mac-mini-Nickolay.local>
2020-10-23 12:55:27 +0300

    #39 session lifetime

--- part4/app.py
+++ part4/app.py
@@ -1,8 +1,11 @@
+import datetime
+
 from flask import Flask, request, make_response, session
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "1213377564b1ff2de1f8e24caddea9f73736cfa1"
-
+# устанавливаем время жизни сессии в 10 дней
+app.permanent_session_lifetime = datetime.timedelta(days=10)
 
 data = [1, 2, 3, 4]
 
@@ -10,6 +13,7 @@ data = [1, 2, 3, 4]
 # Когда в session храним несколько объектов:
 @app.route("/")
 def index():
+    session.permanent = True  # сохраняем сессию после закрытия браузера
     if "data" in session:
         session["data"][0] += 1  # модифицирование данных
         session.modified = True
